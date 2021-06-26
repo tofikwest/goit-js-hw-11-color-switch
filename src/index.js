abe296d51fa486236a949c2729c4ba67daafc76a
Tofik Hasanov <annexcies@gmail.com>
2021-06-26 14:09:05 +0300

    did hw

--- src/index.js
+++ src/index.js
@@ -1,59 +1,32 @@
-import './sass/main.scss';
-import data from './JSON/menu.json';
-import item from './templates/menu.hbs';
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-const ulNode = document.querySelector('ul.js-menu');
-console.log(ulNode);
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function markupMaker() {
-  const tmp = data.map(item).join('');
-  ulNode.innerHTML = tmp;
+const markupColor = document.querySelector("body");
+const onBtnStart = document.querySelector("button[data-action=start]");
+const onBtnStop = document.querySelector("[data-action=stop]");
+
+const DELAY = 1000;
+onBtnStart.addEventListener("click", onChangeStart);
+onBtnStop.addEventListener("click", onChangeStop);
+
+function onChangeStart(fn) {
+  return (setId = setInterval(() => {
+    onBtnStart.setAttribute("disabled", "disabled");
+    markupColor.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, DELAY));
 }
-markupMaker();
-
-const forBody = document.querySelector('body');
-
-const makeDarkorLight = document.querySelector('#theme-switch-toggle');
-
-makeDarkorLight.addEventListener('change', e => {
-  const themeName = e.target.checked ? Theme.DARK : Theme.LIGHT;
-  forBody.classList.add(themeName);
-  forBody.classList.remove(e.target.checked ? Theme.LIGHT : Theme.DARK);
-  localStorage.setItem('Theme', themeName);
-
-  // forBody.classList.toggle(Theme.DARK); // переключатель импута  -рабоатет
-  // const current = localStorage.getItem('Theme');
-  // console.log(current);
-
-  // if (forBody.classList.contains(Theme.DARK)) {
-  //   // если у нас есть Theme Dark на бади то локал сторедж ДАрк
-  //   const asd = localStorage.setItem('Theme', Theme.DARK);
-
-  //   const current = localStorage.getItem('Theme');
-  //   console.log((forBody.classList = current));
-
-  //   if (localStorage.getItem('Theme') === 'dark-theme') {
-  //     // alert('qwe');
-  //     forBody.classList = Theme.DARK;
-  //   }
-
-  //   // localStorage.getItem(Theme.DARK);
-  // } else {
-  //   // в противном случае локал сторедж у нас будет лайт и добавляется клас Лайт
-  //   localStorage.setItem('Theme', Theme.LIGHT);
-  //   // forBody.classList.add(Theme.LIGHT);
-  // }
-});
-
-function inialTheme() {
-  const currTheme = localStorage.getItem('Theme') || Theme.LIGHT;
-  forBody.classList.add(currTheme);
-  if (currTheme === Theme.DARK) {
-    makeDarkorLight.checked = true;
-  }
+function onChangeStop() {
+  clearInterval(setId);
+  onBtnStart.removeAttribute("disabled", "disabled");
 }
-inialTheme();
